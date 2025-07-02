087fc486a92d34241c24760374e7c620ba8ce68e
MainLah <rendrakusumab@gmail.com>
2025-07-02 11:41:39 +0700

    about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,70 @@
-<section id="about" class="flex h-screen items-center justify-center">
+<section id="about" class="flex min-h-screen items-center justify-center bg-lime-950 px-2">
 	<div
-		class="h-[calc(100vh-1rem)] w-[calc(100vw-1rem)] overflow-hidden rounded-[5rem] bg-amber-200"
+		class="relative mx-auto h-[calc(100vh-1rem)] w-full overflow-hidden rounded-[3rem] bg-amber-200 shadow-xl"
 	>
-		<div class="overflow-hidden px-8 whitespace-nowrap">
-			<h1 class="text-[8rem]/none tracking-[2rem] opacity-40">RTFOLIOPORTFOLIOPORTFOLIO</h1>
-			<h1 class="text-[8rem]/none tracking-[2rem]">
-				<span class="opacity-40">RTFOLIO</span><span class="opacity-100">PORTFOLIO</span><span
-					class="opacity-40">PORTFOLIO</span
-				>
+		<div class="pointer-events-none absolute inset-0 hidden select-none md:block">
+			<h1
+				class="w-full text-center text-[6vw] tracking-[2rem] whitespace-nowrap text-black/10 lg:text-[5vw]/none"
+			>
+				RTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIO
 			</h1>
-			<h1 class="text-[8rem]/none tracking-[2rem] opacity-40">RTFOLIOPORTFOLIOPORTFOLIO</h1>
-			<h1 class="text-[8rem]/none tracking-[2rem] opacity-40">RTFOLIOPORTFOLIOPORTFOLIO</h1>
-			<h1 class="text-[8rem]/none tracking-[2rem] opacity-40">RTFOLIOPORTFOLIOPORTFOLIO</h1>
-			<h1 class="text-[8rem]/none tracking-[2rem] opacity-40">RTFOLIOPORTFOLIOPORTFOLIO</h1>
-			<h1 class="text-[8rem]/none tracking-[2rem] opacity-40">RTFOLIOPORTFOLIOPORTFOLIO</h1>
+			<h1
+				class="w-full text-center text-[6vw] tracking-[2rem] whitespace-nowrap text-black/10 lg:text-[5vw]/none"
+			>
+				RTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIO
+			</h1>
+			<h1
+				class="w-full text-center text-[6vw] tracking-[2rem] whitespace-nowrap text-black/10 lg:text-[5vw]/none"
+			>
+				RTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIO
+			</h1>
+			<h1
+				class="w-full text-center text-[6vw] tracking-[2rem] whitespace-nowrap text-black/10 lg:text-[5vw]/none"
+			>
+				RTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIO
+			</h1>
+			<h1
+				class="w-full text-center text-[6vw] tracking-[2rem] whitespace-nowrap text-black/10 lg:text-[5vw]/none"
+			>
+				RTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIO
+			</h1>
+			<h1
+				class="w-full text-center text-[6vw] tracking-[2rem] whitespace-nowrap text-black/10 lg:text-[5vw]/none"
+			>
+				RTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIO
+			</h1>
+			<h1
+				class="w-full text-center text-[6vw] tracking-[2rem] whitespace-nowrap text-black/10 lg:text-[5vw]/none"
+			>
+				RTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIO
+			</h1>
+			<h1
+				class="w-full text-center text-[6vw] tracking-[2rem] whitespace-nowrap text-black/10 lg:text-[5vw]/none"
+			>
+				RTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIO
+			</h1>
+			<h1
+				class="w-full text-center text-[6vw] tracking-[2rem] whitespace-nowrap text-black/10 lg:text-[5vw]/none"
+			>
+				RTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIO
+			</h1>
+			<h1
+				class="w-full text-center text-[6vw] tracking-[2rem] whitespace-nowrap text-black/10 lg:text-[5vw]/none"
+			>
+				RTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIOPORTFOLIO
+			</h1>
+		</div>
+		<div class="relative z-10 flex flex-col items-center justify-center py-16 md:py-32">
+			<h1
+				class="xs:text-4xl text-center text-3xl leading-tight font-extrabold tracking-tight text-black drop-shadow-md sm:text-5xl md:text-6xl lg:text-7xl"
+			>
+				RENDRA KUSUMA B
+			</h1>
+			<h2
+				class="xs:text-2xl mt-4 text-center text-xl font-semibold tracking-wide text-black drop-shadow sm:text-3xl md:text-4xl lg:text-5xl"
+			>
+				A WEB DEVELOPER
+			</h2>
 		</div>
 	</div>
 </section>
