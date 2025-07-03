00b9544e39f0a137f2a89b0d821e33eb501d1658
MainLah <rendrakusumab@gmail.com>
2025-07-03 22:42:21 +0700

    update nav

--- src/lib/components/navItem.svelte
+++ src/lib/components/navItem.svelte
@@ -2,4 +2,4 @@
 	const { text, href } = $props();
 </script>
 
-<li class="rounded-lg px-4 py-2 hover:bg-zinc-300"><a {href}>{text}</a></li>
+<li class="rounded-lg px-4 py-2 hover:bg-lime-900"><a {href}>{text}</a></li>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,11 +11,28 @@
 		{ text: 'Projects', href: '#projects' },
 		{ text: 'Contacts', href: '#contacts' }
 	];
+
+	let showNavbar = $state(false);
+
+	function handleScroll() {
+		const homeSection = document.getElementById('home');
+		if (homeSection) {
+			const rect = homeSection.getBoundingClientRect();
+			showNavbar = rect.bottom <= 0;
+		}
+	}
 </script>
 
-<div class="fixed inset-x-0 top-8 z-50 flex h-[10vh] w-screen items-center justify-center">
-	<nav class="max-w-screen-lg rounded-4xl border border-zinc-300 bg-white px-8 py-3 shadow-lg">
-		<ul class="flex items-center justify-center gap-4">
+<svelte:window onscroll={handleScroll} />
+
+<div
+	class={[
+		'fixed inset-x-0 top-8 z-50 flex h-[10vh] w-screen items-center justify-center opacity-0',
+		{ 'opacity-100': showNavbar }
+	]}
+>
+	<nav class="max-w-screen-lg bg-lime-950 px-8 py-3">
+		<ul class="flex items-center justify-center gap-4 text-amber-200">
 			{#each navItems as item}
 				<NavItem text={item.text} href={item.href} />
 			{/each}
